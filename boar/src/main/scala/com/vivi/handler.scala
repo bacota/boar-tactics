3ddeb354703663180308ff79021795afa6ede25b
Bruce Cota <bruce.cota@komodohealth.com>
2025-02-01 21:17:59 -0500

    REsponseBody

--- boar/src/main/scala/com/vivi/handler.scala
+++ boar/src/main/scala/com/vivi/handler.scala
@@ -1,22 +1,29 @@
 package com.vivi.boar
 
 import com.amazonaws.services.lambda.runtime.Context
-import java.io.{ InputStream, OutputStream, PrintStream }
+import java.io.{InputStream, OutputStream, PrintStream}
 import java.util.Base64
 import scala.io.Source
 import upickle.default._
 import scala.util.Random
 
+case class ResponseBody(
+    value: Double,
+    suggestion: String,
+    values: Map[String, Double]
+)
+
 case class Response(
     statusCode: Int = 200,
-    headers:  Map[String,String] = Map(),
-    body: String
+    headers: Map[String, String] = Map(),
+    body: ResponseBody
 )
 
 class LambdaHandler {
 
     implicit val boarRW: ReadWriter[Problem] = upickle.default.macroRW[Problem]
-    implicit val responseRW: ReadWriter[Response] = upickle.default.macroRW[Response]    
+    implicit val responseBodyRW: ReadWriter[ResponseBody] = upickle.default.macroRW[ResponseBody]
+    implicit val responseRW: ReadWriter[Response] = upickle.default.macroRW[Response]
 
     val random = new Random()
 
@@ -28,22 +35,18 @@ class LambdaHandler {
         val problem: Problem = read[Problem](body)
         val stream = new PrintStream(output)
         val solution = problem.solution.map(_.getOrElse(0.0))
-        val value =solution.last + problem.drm
-        val scan: Seq[Double] = solution.scanLeft(0.0)( (x,y) => x+y)
+        val value = solution.last + problem.drm
+        val scan: Seq[Double] = solution.scanLeft(0.0)((x, y) => x + y)
         val r = random.nextDouble()
-        val suggestion = scan.zip(tactics).reverse.find(_._1 <= r).get._2
-        val values = tactics.zip(solution).map(p => s"${p._1}: ${p._2}").mkString(", ")
+        val responseBody = ResponseBody(
+            value = solution.last + problem.drm,
+            suggestion = scan.zip(tactics).reverse.find(_._1 <= r).get._2,
+            values = tactics.zip(solution).toMap
+        )
         val response = Response(
-            body = s"""
-                 Problem  is ${problem}
-                 Value is ${value}
-                Suggestion  is ${suggestion}
-                Values = ${values}
-           """
+            body = responseBody
         )
         stream.println(write(response))
         output.close()
     }
 }
-
-
